188238f69c336dab0b6fc2883a85ee8a8413c57f
segunsub <segunsubair3@gmail.com>
2021-02-08 14:34:51 -0500

    fine touch

--- src/index.js
+++ src/index.js
@@ -25,36 +25,6 @@ document.querySelector("body").addEventListener("click", document.querySelector(
 
 const { Engine, Render, World, Bounds, Bodies, Body, Constraint, Composites, Composite,  } = Matter;
 
-// const engine = Engine.create();
-// const render = Render.create({
-//   element: document.getElementById("game-container"),
-//   engine: engine,
-//   options: {
-//     width: 800,
-//     height: 600,
-//     wireframes: false, // Draw the shapes as solid colors
-//     background: "#f4f4f8"
-//   }
-// });
-
-
-
-// // Create a rectangle centered at the top of the screen, (400, 0), with 120px width and 80px height
-// const rectangle = Bodies.rectangle(400, 0, 120, 80, { restitution: 0.25, angle: Math.PI / 4 });
-
-// // Create an immovable rectangle at the bottom of the screen that will act as the floor
-// const floor = Bodies.rectangle(400, 575, 800, 50, { isStatic: true });
-
-// // Add the newly minted bodies to our physics simulation
-// World.add(engine.world, [rectangle, floor]);
-
-// // Kick off the simulation and the render loops
-// Engine.run(engine);
-// Render.run(render);
-
-
-
-
 
 // // create engine
 const engine = Engine.create(), world = engine.world;
@@ -90,23 +60,6 @@ World.add(world, [
 ]);
 
 
-
-
-// // get the centre of the viewport
-// var viewportCentre = {
-//   x: render.options.width * 0.5,
-//   y: render.options.height * 0.5
-// };
-
-//     // make the world bounds a little bigger than the render bounds
-//     world.bounds.min.x = -300;
-//     world.bounds.min.y = -300;
-//     world.bounds.max.x = 1100;
-//     world.bounds.max.y = 900;
-
-
-
-
 document.addEventListener('keydown', function(event) {
   const key = event.key;
   switch (event.key) {
@@ -130,14 +83,3 @@ let update = setInterval(()=>{
     x: 900,
     y: 400,
   }, true)}, 1);
-
-
-
-// for camera experiment
-// const viewportCentre = {
-//   x: render.options.width * 0.5,
-//   y: render.options.height * 0.5
-// };
-// var star = Matter.Vertices.create(viewportCentre)
-// let translate = {x : 0, y : 0}
-// Bounds.translate(render.bounds, translate);
