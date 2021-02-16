fdd969f417bd2af3df2117e710a6d862ef576bb6
segunsub <segunsubair3@gmail.com>
2021-02-16 14:21:34 -0500

    switch to buttons

--- src/index.js
+++ src/index.js
@@ -236,19 +236,19 @@ let lowfuel = setInterval(()=>{
 //Youtube Api Audio
 function onYouTubeIframeAPIReady() {
   var e=document.getElementById("youtube-audio"),
-  t=document.createElement("img");t.setAttribute("id","youtube-icon"),
+  t=document.createElement("button");t.setAttribute("id","youtube-icon"),
   t.style.cssText="cursor:pointer;cursor:hand",
   e.appendChild(t);
   var a=document.createElement("div");
   a.setAttribute("id","youtube-player"),
   e.appendChild(a);
-  var o=function(e){var a=e?"IDzX9gL.png":"quyUPXN.png";
-  t.setAttribute("src","https://i.imgur.com/"+a)};
+  console.log(t)
+  t.innerText = 'Play'
   setInterval(()=>{
     r.setVolume(musicslide.value)
   }, 1000)
-  e.onclick=function(){r.getPlayerState()===YT.PlayerState.PLAYING||r.getPlayerState()===YT.PlayerState.BUFFERING?(r.pauseVideo(),o(!1)):(r.playVideo(),o(!0))};
+  e.onclick=function(){r.getPlayerState()===YT.PlayerState.PLAYING||r.getPlayerState()===YT.PlayerState.BUFFERING?(r.pauseVideo(),(t.innerText = 'Play')):(r.playVideo(),(t.innerText = 'Pause'))};
 
-  var r=new YT.Player("youtube-player",{height:"0",width:"0",videoId:e.dataset.video,volume:e.dataset.volume,playerVars:{autoplay:e.dataset.autoplay,loop:e.dataset.loop},events:{onReady:function(e){r.setPlaybackQuality("small"),o(r.getPlayerState()!==YT.PlayerState.CUED)},onStateChange:function(e){e.data===YT.PlayerState.ENDED&&o(!1)}}})
+  var r=new YT.Player("youtube-player",{height:"0",width:"0",videoId:e.dataset.video,volume:e.dataset.volume,playerVars:{autoplay:e.dataset.autoplay,loop:e.dataset.loop},events:{onReady:function(e){r.setPlaybackQuality("small"),(r.getPlayerState()!==YT.PlayerState.CUED)},onStateChange:function(e){e.data===YT.PlayerState.ENDED&&o(!1)}}})
 
 }
